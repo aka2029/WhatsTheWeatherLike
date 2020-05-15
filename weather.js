3c7ace2daf0d18562ede5f6e15f465e42e8f4457
aka2029 <asaks22@gmail.com>
2020-05-15 23:34:48 +0530

    new commit

---
+++ weather.js
@@ -0,0 +1,24 @@
+class Weather {
+  constructor(city, state) {
+    this.apiKey = "a90e42f0c8c1f92ac62cd97bd63f24a0";
+    this.city = city;
+    this.state = state;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q={${this.city}},{${this.state}}&appid={${this.apiKey}}`
+    );
+
+    const reponseData = await response.json();
+
+    return reponseData;
+  }
+
+  // Change weather location
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
